df52aefffa9be5938a59390b9612ed3dc1793dba
Lucho-lja <ljallauca.a@gmail.com>
2022-05-07 18:52:00 -0500

    Add edit view and #edit, #update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to new_user_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
     def create
         #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
         @user = User.new(user_params)
@@ -12,6 +23,7 @@ class UsersController < ApplicationController
             render :new, status: :unprocessable_entity
         end
     end
+    
     
     def user_params
         params.require(:user).permit(:username, :email, :password)
